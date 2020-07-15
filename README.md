1350f2625f621bcb5934eacfee27e0895308c483
Dominic Wienzek <dominic.wienzek@gmail.com>
2020-07-15 11:44:48 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# SpotMyBackupReverse
+
+A little playlist reverser for the useful [SpotMyBackup](http://www.spotmybackup.com) tool.
+
+SpotMyBackup is a nice tool to import and export your [Spotify](https://www.spotify.com) playlists from one account to another.
+The only problem is that it imports the tracks in the wrong direction, so
+that the newest tracks are at the top of the playlist, which is not similar to Spotify.
+This tool fixes this and reorders the tracks in the backup file.
+
+Simply use SpotMyBackupReverse by running the following at the command line:
+`java -jar SpotMyBackupReverse.jar <file>`
+The file attribute should be replaced with your backup json file.
+
+## Table of Contents
+
+* [Authors](#authors--contributors)
+* [License](#license)
+
+## Authors & Contributors
+
+* **Dominic Wienzek** - *Development* - [Skyleiger](https://github.com/skyleiger)
+
+See also the list of [contributors](https://github.com/Skyleiger/SpotMyBackupReverse/contributors) who participated in this project.
+
+## License
+
+This project is is the property of the author(s) and may only be used with the consent of the author(s). Other uses are not permitted.
---
+++ src/main/java/de/dwienzek/spotmybackupreverse/SpotMyBackupReverse.java
@@ -0,0 +1,75 @@
+package de.dwienzek.spotmybackupreverse;
+
+import com.google.gson.*;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class SpotMyBackupReverse {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please enter file argument. Example: 'java -jar SpotMyBackupReverse.jar spotifybackup.json'");
+            System.exit(0);
+        }
+
+        new SpotMyBackupReverse(new File(args[0]));
+    }
+
+    public SpotMyBackupReverse(@NonNull File file) {
+        if (!file.exists()) {
+            System.out.println("The given file does not exists.");
+            System.exit(0);
+        }
+
+        if (!file.isFile()) {
+            System.out.println("The given file is not a file.");
+            System.exit(0);
+        }
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try {
+            System.out.println("Reading file...");
+            FileReader reader = new FileReader(file);
+            JsonObject jsonFile = gson.fromJson(reader, JsonObject.class);
+            reader.close();
+            System.out.println("File read complete.");
+
+            System.out.println("Reordering playlist tracks...");
+            JsonObject oldPlaylists = jsonFile.getAsJsonObject("playlists");
+            JsonObject newPlaylists = new JsonObject();
+            oldPlaylists.entrySet().forEach(entry -> {
+                System.out.println("Reordering playlist '" + entry.getKey() + "'...");
+                JsonObject currentPlaylist = entry.getValue().getAsJsonObject();
+                JsonArray oldTracks = currentPlaylist.getAsJsonArray("tracks");
+                List<JsonElement> reorderedTracks = new ArrayList<>(oldTracks.size());
+                oldTracks.forEach(element -> reorderedTracks.add(0, element));
+
+                JsonArray newTracks = new JsonArray(reorderedTracks.size());
+                reorderedTracks.forEach(newTracks::add);
+                currentPlaylist.add("tracks", newTracks);
+                newPlaylists.add(entry.getKey(), currentPlaylist);
+                System.out.println("Reordered playlist '" + entry.getKey() + "'.");
+            });
+            jsonFile.add("playlists", newPlaylists);
+            System.out.println("Reorder complete.");
+
+            System.out.println("Writing file...");
+            FileWriter writer = new FileWriter(file);
+            gson.toJson(jsonFile, writer);
+            writer.close();
+            System.out.println("File write complete.");
+        } catch (IOException exception) {
+            System.err.println("Failed to handle file:");
+            exception.printStackTrace();
+        }
+    }
+}
